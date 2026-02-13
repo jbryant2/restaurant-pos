e8e134710d616f085cb37170ce2948582d57fc60
jbryant2 <bryant.jakob@gmail.com>
2026-02-13 14:00:44 -0500

    Add Spring Boot backend scaffolding

---
+++ backend/src/test/java/com/restaurantpos/backend/RestaurantPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantpos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
